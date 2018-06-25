848bca45671f04f695a7cab34ad12ef89b045bce
V0V <grapduck@gmail.com>
2018-06-25 20:16:47 +0900

    문열기,손전등 줍기
    InteractWithDoorStateScript(카메라에 적용),DoorState(열고 싶은 문에 적용)
    BeingHas(손전등에 적용)

---
+++ RoomEscape.UI/Assets/BeingHas.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BeingHas : MonoBehaviour
+{
+
+    public SphereCollider sphereCollider;
+   
+    bool PlayerIn;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.transform.name);
+        PlayerIn = true;
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+        PlayerIn = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && PlayerIn)
+        {
+            transform.parent = GameObject.Find("Player").transform;
+            Debug.Log("Flashlight");
+            transform.localPosition = new Vector3(0.5f,0.5f,0.5f);
+            sphereCollider.enabled = false;
+
+
+
+
+        }
+
+
+
+    }
+
+    
+
+}
---
+++ RoomEscape.UI/Assets/InteractWithDoorStateScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithDoorStateScript : MonoBehaviour
+{
+
+    public float interactDistance = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, interactDistance))
+            {
+                if (hit.collider.CompareTag("Door"))
+                {
+                    hit.collider.transform.GetComponent<DoorState>().ChangeDoorState();
+                }
+                ///if(hit.collider.CompareTag("Flashlight"))
+                ///{
+                ///    hit.collider.transform.GetComponent<BeingHas>().ChangeState();
+                ///}
+            }
+        }
+    }
+}
